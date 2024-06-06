611eedcde7d868bfe79fb262855b48dd207e3a56
prshptl <inb252parpat@CSPLAPPUN642.ext.crifnet.com>
2024-06-06 18:11:44 +0530

    First Commit.....

---
+++ src/Sample/TestProgram.java
@@ -0,0 +1,21 @@
+package Sample;
+
+public class TestProgram {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String hungry = "Yes";
+		
+		if(hungry == "Yes")
+		{
+			System.out.println("Eat Something");
+		}
+		else
+		{
+			System.out.println("Do Work");
+		}
+
+	}
+
+}
